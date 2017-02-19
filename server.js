e07d0012bafd51f2459c9a4aa0d12805bcd09d4d
Caity Kondo <caitykondo@Caitys-MacBook-Pro-2.local>
2017-02-18 16:27:07 -1000

    can use node module for redis

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const handlebars = require('express-handlebars');
+const cacheMaster = require('./cacheMaster');
 
 const { slow } = require('./routes');
 
 const app = express();
 const PORT = 8080;
 
+const redis = require('redis');
+client = redis.createClient();
+
 app.engine('.hbs', handlebars({extname: '.hbs', defaultLayout: 'main'}));
 app.set('view engine', '.hbs');
 
 app.use(bodyParser.json());
+
+app.use(cacheMaster.get);
 // server.use(creamCache.init()); /* student implements this */
 app.use('/slow', slow);
 
