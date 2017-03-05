9ab5ba29b9f19b4571f3d8214fe354f8538282fd
Caity Kondo <caitykondo@Caitys-MacBook-Pro-2.local>
2017-03-04 14:30:00 -1000

    reset server.js to match example

--- server.js
+++ server.js
@@ -7,29 +7,17 @@ const { slow } = require('./routes');
 const app = express();
 const PORT = 8080;
 
-const redis = require('redis');
-client = redis.createClient();
-
-const cache = require('express-redis-cache')({client: client, expire: 60});
-
 app.engine('.hbs', handlebars({extname: '.hbs', defaultLayout: 'main'}));
 app.set('view engine', '.hbs');
 
 app.use(bodyParser.json());
-
 // server.use(creamCache.init()); /* student implements this */
-
-app.use('/slow', cache.route(), slow);
-app.use('/api', cache.route(), slow);
+app.use('/slow', slow);
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-if(!module.parent){
-  app.listen(PORT, () => {
-    process.stdout.write(`server listening on port ${PORT}`);
-  });
-}
-
-module.exports = app;
+app.listen(PORT, () => {
+  process.stdout.write(`server listening on port ${PORT}`);
+});
