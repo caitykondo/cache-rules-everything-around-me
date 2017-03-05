865e9e1674f4bfb6d5d250ece85a731b63c47d21
Caity Kondo <caitykondo@Caitys-MacBook-Pro-2.local>
2017-03-04 15:35:59 -1000

    uninstalled express-redis-cache, using node-redis instead

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const handlebars = require('express-handlebars');
-
 const { slow } = require('./routes');
-
 const app = express();
 const PORT = 8080;
+const redis = require('redis');
+
+const client = redis.createClient();
+
+client.on('connect', ()=>{
+  console.log('\nconnected client to redis');
+});
+
 
 app.engine('.hbs', handlebars({extname: '.hbs', defaultLayout: 'main'}));
 app.set('view engine', '.hbs');
 
 app.use(bodyParser.json());
 // server.use(creamCache.init()); /* student implements this */
+
+
+client.set('1', '1st');
+
+
 app.use('/slow', slow);
 
 app.get('/', (req, res) => {
