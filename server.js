c6f1f73e27b40bd241ec83f1bf47bad0c014d68a
Caity Kondo <caitykondo@Caitys-MacBook-Pro-2.local>
2017-03-04 15:37:15 -1000

    can checkCache on / route

--- server.js
+++ server.js
@@ -19,14 +19,28 @@ app.set('view engine', '.hbs');
 app.use(bodyParser.json());
 // server.use(creamCache.init()); /* student implements this */
 
+function checkCache(req, res, next) {
+  console.log('checking cache');
+  let path = req.route.path;
+  client.get(req.route.path, (err, reply) =>{
+    if(reply !== null){
+      console.log('found in cache');
+      res.send(reply);
+    }else{
+      next();
+      // render the page and store in cache
+    }
 
-client.set('1', '1st');
-
+  });
+}
 
 app.use('/slow', slow);
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', checkCache, (req, res) => {
+  res.render('index', (err, html) => {
+    console.log('setting to cache');
+    client.set(req.route.path, html);
+  });
 });
 
 app.listen(PORT, () => {
